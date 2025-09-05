f8b489ebae349ab610dd648fd172fa366552950f
lerraisonestudio <rodriguezcastroisrrael@gmail.com>
2025-09-05 10:31:12 -0400

    config.py

--- FlyCloudOne_FULL_v2/config/config.py
+++ FlyCloudOne_FULL_v2/config/config.py
@@ -1,15 +1,12 @@
 import os
 
 class Config:
-    
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecreto123")
 
+    # Configuración Mail
     MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
-    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() in ["true", "1", "t"]
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", MAIL_USERNAME)
-
-    SECRET_KEY = os.getenv("SECRET_KEY", "cambia_esto_por_algo_muy_secreto")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
